6ccc2254b128d997b3ed8d21701b8870c117fc24
zmiguel2011 <93340308+zmiguel2011@users.noreply.github.com>
2022-04-20 16:25:10 +0100

    add timer for program running time

--- Otimizacao.cpp
+++ Otimizacao.cpp
@@ -16,13 +16,14 @@ Otimizacao::Otimizacao(Armazem &a1): armazem(a1) {
 int Otimizacao::cenario2() {
     MaxHeap<int, int> recompensa = makemaxheap_recompensa();
     MinHeap<int, int> custo = makeminheap_custo();
-    int lucrototal = 0, lucro;
+    int lucrototal = 0, lucro, n_estafetas = 0, n_encomendas = 0;
     while (lucrototal >= 0) {
         lucro = 0;
         int est_id = custo.removeMin().first;
         Estafeta est = estafetas.find(est_id)->second;
         int est_vol = est.getvolmax();
         int est_peso = est.getpesomax();
+        n_estafetas++;
         while (est_vol >= 0 && est_peso >=0) {
             int enc_id = recompensa.removeMax().first;
             Encomenda enc = encomendas.find(enc_id)->second;
@@ -30,6 +31,7 @@ int Otimizacao::cenario2() {
                 est_vol -= enc.getvolume();
                 est_peso -= enc.getpeso();
                 lucro += enc.getrecompensa();
+                n_encomendas++;
             }
             else {
                 recompensa.insert(enc_id, enc.getrecompensa() / (enc.getvolume() + enc.getpeso()));
@@ -40,6 +42,9 @@ int Otimizacao::cenario2() {
         if (lucro <= 0) { break; }
         lucrototal += lucro;
     }
+    std::cout << "Estafetas Utilizados: " << n_estafetas << endl;
+    std::cout << "Encomendas Entregues: " << n_encomendas << endl;
+    std::cout << "Lucro da Empresa: ";
     return lucrototal;
 }
 
@@ -77,4 +82,17 @@ MinHeap<int, int> Otimizacao::makeminheap() {
         treat.insert(it.first, it.second.gettempoentrega());
     }
     return treat;
+}
+
+int Otimizacao::getn(int n) {
+    switch (n) {
+        case 1:
+            return estafetas.size();
+        case 2:
+            return encomendas.size();
+        case 3:
+            return expresso.size();
+        default:
+            return -1;
+    }
 }
